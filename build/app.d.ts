e97ae70e6a5115b70655765b77bf656b8fbf2e3f
BGKim <embc32@hotmail.com>
2022-05-07 22:52:32 +0900

    typescript : lint

---
+++ build/app.d.ts
@@ -0,0 +1,2 @@
+export = app;
+declare var app: import("express-serve-static-core").Express;
---
+++ build/modules/example.js
@@ -0,0 +1,4 @@
+"use strict";
+const say = "Hello World";
+console.log(say);
+//# sourceMappingURL=example.js.map
